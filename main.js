80a8aa053c6d2a0e4cbd0a8a241c3636dcda5448
kiripi <qostrackes@gmail.com>
2023-02-14 16:39:36 +0100

    add working functions, complete event handlers

--- main.js
+++ main.js
@@ -2,22 +2,77 @@
 const numBtn = document.querySelectorAll(".num");
 const operatorBtn = document.querySelectorAll(".operator");
 const clearBtn = document.querySelector(".clear");
+const equalBtn = document.querySelector(".equals");
+const deleteBtn = document.querySelector(".delete");
 
 // Display
-const firstNumber = document.querySelector(".number");
+const firstNumber = document.querySelector(".number p");
 const secondNumber = document.querySelector(".result");
-const operator = document.querySelector(".operator");
+const operatorSign = document.querySelector(".sign");
+
+let result = "";
 
 // Functions
 function changeNumber() {
   secondNumber.innerHTML += this.textContent;
 }
 
-function operate() {}
+function operate() {
+  firstNumber.innerHTML = secondNumber.innerHTML;
+  operatorSign.innerHTML = this.textContent;
+  secondNumber.innerHTML = "";
+}
 
-function result() {}
+function showResult() {
+  let x = Number(secondNumber.innerHTML);
+  let y = Number(firstNumber.innerHTML);
+  let operator = operatorSign.innerHTML;
+
+  switch (operator) {
+    case "+":
+      result = x + y;
+      break;
+    case "-":
+      result = y - x;
+      break;
+    case "*":
+      result = x * y;
+      break;
+    case "/":
+      result = y / x;
+      break;
+    case "^":
+      result = y ** x;
+      break;
+    case "%":
+      result = y % x;
+      break;
+  }
+  secondNumber.innerHTML = result;
+  firstNumber.innerHTML = "";
+  operatorSign.innerHTML = "";
+}
+
+function deleteLastChar() {
+  secondNumber.innerHTML = secondNumber.innerHTML.slice(0, -1);
+}
+
+function clearDisplay() {
+  result = "";
+  firstNumber.innerHTML = "";
+  secondNumber.innerHTML = "";
+  operatorSign.innerHTML = "";
+}
 
 // Event Listeners
+operatorBtn.forEach((button) => button.addEventListener("click", operate));
+
+equalBtn.addEventListener("click", showResult);
+
+clearBtn.addEventListener("click", clearDisplay);
+
+deleteBtn.addEventListener("click", deleteLastChar);
+
 numBtn.forEach((button) => {
   button.addEventListener("click", changeNumber);
 });
